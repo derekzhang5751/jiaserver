819cd00bfe256804f9a28403727562936a259987
Derek Zhang <derekzhang5751@gmail.com>
2018-02-08 14:54:36 -0500

    20180208-2 normal commit

--- common/JiaSession.php
+++ common/JiaSession.php
@@ -11,26 +11,26 @@ class JiaSession implements \Bricker\ISession
     var $db             = NULL;
     var $session_table  = 'sessions';
     var $session_data_table  = 'sessions_data';
-
+    
     var $max_life_time  = 1800;
-
+    
     var $session_name   = 'ECS_ID';
     var $session_id     = '';
-
+    
     var $session_expiry = '';
     var $session_md5    = '';
-
+    
     var $session_cookie_path   = '/';
     var $session_cookie_domain = '';
     var $session_cookie_secure = false;
-
+    
     var $_ip   = '';
     var $_time = 0;
-
+    
     public function init($db, $session_id = '') {
         //$GLOBALS['log']->log('bricker', 'JiaSession init with session id = '.$session_id);
         $GLOBALS['_SESSION'] = array();
-
+        
         if ( !empty($GLOBALS['cookie_path']) ) {
             $this->session_cookie_path = $GLOBALS['cookie_path'];
         } else {
@@ -83,27 +83,27 @@ class JiaSession implements \Bricker\ISession
         
         register_shutdown_function(array($this, 'close_session'));
     }
-
+    
     public function response() {
         //
     }
-
+    
     private function gen_session_id() {
         $this->session_id = md5(uniqid(mt_rand(), true));
         
         return $this->insert_session();
     }
-
+    
     private function gen_session_key($session_id) {
         static $ip = '';
-
+        
         if ($ip == '') {
             $ip = substr($this->_ip, 0, strrpos($this->_ip, '.'));
         }
-
+        
         return sprintf('%08x', crc32(ROOT_PATH . $ip . $session_id));
     }
-
+    
     private function insert_session() {
         $params = array(
             'sesskey' => $this->session_id,
@@ -113,18 +113,18 @@ class JiaSession implements \Bricker\ISession
         );
         return db_insert_session($params);
     }
-
+    
     private function load_session() {
         $session = db_get_session($this->session_id);
         if (empty($session)) {
             $this->insert_session();
-
+            
             $this->session_expiry = 0;
             $this->session_md5    = '40cd750bba9870f18aada2478b24840a';
             $GLOBALS['_SESSION']  = array();
-
+            
         } else {
-            if (!empty($session['data']) && $this->_time - $session['expiry'] <= $this->max_life_time)
+            if (!empty($session['data']) /*&& $this->_time - $session['expiry'] <= $this->max_life_time*/ )
             {
                 $this->session_expiry = $session['expiry'];
                 $this->session_md5    = md5($session['data']);
@@ -160,7 +160,7 @@ class JiaSession implements \Bricker\ISession
             }
         }
     }
-
+    
     private function update_session() {
         $adminid   = !empty($GLOBALS['_SESSION']['admin_id']) ? intval($GLOBALS['_SESSION']['admin_id']) : 0;
         $userid    = !empty($GLOBALS['_SESSION']['user_id'])  ? intval($GLOBALS['_SESSION']['user_id'])  : 0;
@@ -205,7 +205,7 @@ class JiaSession implements \Bricker\ISession
         db_update_sessioin($this->session_id, $params);
         return true;
     }
-
+    
     function close_session() {
         $this->update_session();
         
@@ -223,23 +223,23 @@ class JiaSession implements \Bricker\ISession
         
         return true;
     }
-
+    
     public function deleteSession()
     {
         $GLOBALS['_SESSION'] = array();
-
+        
         setcookie($this->session_name, $this->session_id, 1, $this->session_cookie_path, $this->session_cookie_domain, $this->session_cookie_secure);
-
+        
         if (!empty($GLOBALS['ecs']))
         {
             $this->db->query('DELETE FROM ' . $GLOBALS['ecs']->table('cart') . " WHERE session_id = '$this->session_id'");
         }
-
+        
         $this->db->query('DELETE FROM ' . $this->session_data_table . " WHERE sesskey = '" . $this->session_id . "' LIMIT 1");
-
+        
         return $this->db->query('DELETE FROM ' . $this->session_table . " WHERE sesskey = '" . $this->session_id . "' LIMIT 1");
     }
-
+    
     public function getSessionId()
     {
         return $this->session_id;
